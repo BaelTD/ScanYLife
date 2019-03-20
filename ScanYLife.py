f5c39347fad7a536f268ba95c6975075034f3b60
bael <morelli.d14@gmail.com>
2019-03-20 21:47:42 +0100

    ScanYLife.py

---
+++ ScanYLife.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import os
+
+def sexScan (): 
+	scan_Successfully = None;
+
+
+	return scan_Successfully
+
+def onlyUP(ip,port):
+	scan_Up = None;
+
+	
+	return scan_Up
+
+
+if __name__ == '__main__' :
+	
+	
